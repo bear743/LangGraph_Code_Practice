7011ede756f36cb444e610dbd6aeb1f77d371327
bear743 <1181472525@qq.com>
2025-09-25 11:22:34 +0800

    added agent project 04

---
+++ Agent/04_react_agent_drafter.py
@@ -0,0 +1,132 @@
+from dotenv import load_dotenv
+from typing import TypedDict, Sequence, Annotated
+from langchain_core.messages import HumanMessage, ToolMessage, SystemMessage, BaseMessage
+from langchain_core.tools import tool
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.graph import StateGraph, END
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode
+
+load_dotenv()
+
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+
+document_content = ""  # global variable for storing document content
+
+@tool
+def update(content: str) -> str:
+    """ Updates the document with the provided content """
+    global document_content
+    document_content = content
+    return f"Document has been updated successfully!  The current content is:\n{document_content}"
+
+@tool
+def save(filename: str) -> str:
+    """Save the current document to a text file and finish the process.
+    
+    Args:
+        filename: Name for the text file.
+    """
+    global document_content
+
+    if not filename.endswith(".txt"):
+        filename = f"{filename}.txt"
+    
+    try:
+        with open(filename, 'w') as file:
+            file.write(document_content)
+        print(f"\n💾 Document has been saved to: {filename}")
+        return f"Document has been saved successfully to '{filename}'."
+    except Exception as e:
+        return f"Error saving document: {str(e)}"
+
+tools = [update, save, ]
+
+
+llm_with_tools = ChatGoogleGenerativeAI(model="gemini-2.5-flash").bind_tools(tools=tools)
+
+def agent_node(state: AgentState):
+    system_prompt = SystemMessage(content=f"""
+    You are Drafter, a helpful writing assistant. You are going to help the user write, modify and save documents.
+    
+    - If the document content is empty in the beginning, output: "I'm ready to help you update a document. What would you like to create?" to the user.
+    - If the user wants to create or modify content, use the 'update' tool with the complete updated content.
+    - If the user wants to save and finish, you need to use the 'save' tool.
+    - Make sure to always show the current document content after modifications.
+    
+    The current document content is:{document_content}
+    """)
+    if not state["messages"]:
+        user_input = "Hello, let's start writing!"
+        user_message = HumanMessage(content=user_input)
+    else:
+        user_input = input("\nWhat would you like to do with the document? ")
+        print(f"\n👤 USER: {user_input}")
+        user_message = HumanMessage(content=user_input)
+
+    all_messages = [system_prompt] + list(state["messages"]) + [user_message]
+
+    response = llm_with_tools.invoke(all_messages)
+
+    print(f"\n🤖 AI: {response.content}")
+    if hasattr(response, "tool_calls") and response.tool_calls:
+        print(f"🔧 USING TOOLS: {[tc['name'] for tc in response.tool_calls]}")
+    
+    return AgentState(messages=[user_message, response])
+
+def should_continue(state: AgentState) -> str:
+    """Determine if we should continue or end the conversation."""
+    messages = state["messages"]
+    
+    if not messages:
+        return "continue"
+    
+    # This looks for the most recent tool message....
+    for message in reversed(messages):
+        # ... and checks if this is a ToolMessage resulting from save
+        if (isinstance(message, ToolMessage) and 
+            "saved" in message.content.lower() and
+            "document" in message.content.lower()):
+            return "end" # goes to the end edge which leads to the endpoint
+        
+    return "continue"
+
+def print_messages(messages):
+    """Function to print the messages in a more readable format"""
+    if not messages:
+        return
+    
+    for message in messages[-3:]:
+        if isinstance(message, ToolMessage):
+            print(f"\n🛠️ TOOL RESULT: {message.content}")
+
+
+graph = StateGraph(AgentState)
+graph.add_node("agent", agent_node)
+graph.add_node("tools", ToolNode(tools))
+graph.set_entry_point("agent")
+graph.add_edge("agent", "tools")
+graph.add_conditional_edges(
+    "tools",
+    should_continue,
+    {
+        "continue": "agent",
+        "end": END,
+    },
+)
+app = graph.compile()
+
+def run_document_agent():
+    print("\n ===== DRAFTER =====")
+    
+    state = {"messages": []}
+    
+    for step in app.stream(state, stream_mode="values"):
+        if "messages" in step:
+            print_messages(step["messages"])
+    
+    print("\n ===== DRAFTER FINISHED =====")
+
+if __name__ == "__main__":
+    run_document_agent()
